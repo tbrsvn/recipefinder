61bd4d61e388c770971dfe905defbf53a9fadbff
Toma Brasoveanu <104178438+Norok-The-Diablo@users.noreply.github.com>
2023-09-02 11:45:24 -0700

    Version 1.0 Recipe Finder

--- RecipeFinder.py
+++ RecipeFinder.py
@@ -3,43 +3,36 @@ import tkinter as tk
 from tkinter import simpledialog
 from gooey import Gooey, GooeyParser
 import ujson as json
+import pkg_resources
+from autocorrect import Speller
 
+spell = Speller()
 tokenizer = RegexpTokenizer(r"\w+")
 
-# Load recipe data from a single JSON file
-def load_recipe_data(file_path):
-    with open(file_path, "r") as file:
-        recipe_data = json.load(file)
-    return recipe_data
+def load_recipe_data():
+    try:
+        with pkg_resources.resource_stream(__name__, 'recipes.json') as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"Error loading recipe data: {e}")
+        return {}
 
-# Filter recipes based on available ingredients
 def find_matching_recipes(recipe_data, available_ingredients):
     matching_recipes = []
+
     for recipe_key, recipe_info in recipe_data.items():
-        recipe = recipe_info["json"]
-        recipe_ingredients = tokenizer.tokenize(" ".join(recipe["ingredients"]))
+        recipe = recipe_info
+        recipe_ingredients = tokenizer.tokenize(" ".join(recipe.get("ingredients", [])))
         recipe_ingredients = [ingredient.lower() for ingredient in recipe_ingredients]
+
+        additional_ingredients = [ingredient for ingredient in recipe_ingredients if ingredient not in available_ingredients]
+        score = len(additional_ingredients)
+
         if all(keyword in recipe_ingredients for keyword in available_ingredients):
-            matching_recipes.append(recipe)
+            matching_recipes.append({"recipe": recipe, "score": score})
+
+    matching_recipes.sort(key=lambda x: x["score"])
     return matching_recipes
-
-# Display matching recipes
-def display_recipes(recipes):
-    if not recipes:
-        print("No matching recipes found.")
-    else:
-        print("Matching recipes:")
-        for index, recipe in enumerate(recipes, start=1):
-            print(f"{index}. {recipe['title']}", flush=True)
-
-def display_recipe_details(recipe):
-    print(f"\nRecipe: {recipe['title']}\n")
-    print("Ingredients:")
-    for ingredient in recipe["ingredients"]:
-        print(f"- {ingredient}")
-    print("\nDirections:")
-    for step, direction in enumerate(recipe["directions"], start=1):
-        print(f"{step}. {direction}")
 
 @Gooey(program_name="Recipe Finder", default_size=(800, 600))
 def main():
@@ -49,27 +42,39 @@ def main():
 
     args = parser.parse_args()
 
-    Available_Ingredients = [ingredient.strip().lower() for ingredient in args.Available_Ingredients.split(",")]
+    input_ingredients = args.Available_Ingredients.split(",")
+    corrected_ingredients = [spell(ingredient.strip().lower()) for ingredient in input_ingredients]
 
-    # Load recipe data from the specified JSON file
-    recipe_data = load_recipe_data("recipes.json")
+    recipe_data = load_recipe_data()
 
-    # Find matching recipes
-    matching_recipes = find_matching_recipes(recipe_data, Available_Ingredients)
+    if not recipe_data:
+        return
 
-    # Display matching recipes
-    display_recipes(matching_recipes)
+    matching_recipes = find_matching_recipes(recipe_data, corrected_ingredients)
 
-    if matching_recipes:
-        root = tk.Tk()
-        root.iconbitmap("icon.ico")
-        root.withdraw()  # Hide the main GUI window
-        recipe_number = simpledialog.askinteger("Recipe Selection", "Enter the number of the recipe you want to view:")
-        if recipe_number is not None and 0 <= recipe_number - 1 < len(matching_recipes):
-            selected_recipe = matching_recipes[recipe_number - 1]
-            display_recipe_details(selected_recipe)
-        elif recipe_number is not None:
-            print("Invalid recipe number.")
+    if not matching_recipes:
+        print("No matching recipes found.")
+        return
+
+    print("\nMatching recipes (Sorted by least additional ingredients, lower is better):")
+    for index, recipe in enumerate(matching_recipes, start=1):
+        print(f"{index}. {recipe['recipe'].get('title', 'Title not available')} (Score: {recipe['score']})")
+
+    root = tk.Tk()
+    root.withdraw()
+    recipe_number = simpledialog.askinteger("Recipe Selection", "Enter the number of the recipe you want to view:")
+    
+    if recipe_number is not None and 0 <= recipe_number - 1 < len(matching_recipes):
+        selected_recipe = matching_recipes[recipe_number - 1]["recipe"]
+        print(f"\nRecipe: {selected_recipe.get('title', 'Title not available')}\n")
+        print("Ingredients:")
+        for ingredient in selected_recipe.get("ingredients", []):
+            print(f"- {ingredient}")
+        print("\nDirections:")
+        for step, direction in enumerate(selected_recipe.get("directions", []), start=1):
+            print(f"{step}. {direction}")
+    elif recipe_number is not None:
+        print("\nInvalid recipe number.")
 
 if __name__ == "__main__":
     main()
